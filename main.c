a18c2237e947139b0e3b003c501a7750089db26d
Maxim Vostretsov <vostretsov0505@gmail.com>
2020-12-19 21:39:39 +0400

    Fix function names and make the code work properly.

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
+#include <unistd.h>
 
 struct termios orig_termios;
 
-void disable_raw_mode(){
-    int tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+void disable_raw_mode() {
+      tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
-void enable_raw_mode(){
-    int tcgetattr(STDIN_FILENO, &orig_termios);
+void enable_raw_mode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disable_raw_mode);
     struct termios raw = orig_termios;
 
@@ -15,3 +19,16 @@ void enable_raw_mode(){
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+
+int main() {
+    enable_raw_mode();
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
+    return 0;
+    }
