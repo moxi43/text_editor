53a6275f5d9a4bf2c8b4d3aea2ddb713dd55ee06
Maxim Vostretsov <vostretsov0505@gmail.com>
2020-12-20 21:31:41 +0400

    Cleared main function. Start to preprocess the terminal to using ncurses library.

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 #include <termios.h>
 #include <unistd.h>
 
+///////////------defines---////////////
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 ///////////------data------////////////
 struct termios orig_termios;
 
@@ -38,21 +41,40 @@ void enable_raw_mode() {
         die("tcsetattr");
 }
 
+char editor_read_key() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN)
+            die("read");
+    }
+    return c;
+}
+
+///////////----output------////////////
+
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+}
+
+///////////-----input------////////////
+
+void editor_process_keypress() {
+    char c = editor_read_key();
+
+    switch (c) {
+        case CTRL_KEY('q'):
+            exit(0);
+            break;
+    }
+}
+
 ///////////------init------////////////
 
 int main() {
     enable_raw_mode();
     while (1) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-            die("read");
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == 'q')
-            break;
+        editor_process_keypress();
     }
     return 0;
     }
